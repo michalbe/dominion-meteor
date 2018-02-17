60e489045b92bff0049b81c2a06b8fd06a7d9cff
Michał Budzyński <michal@virtualdesign.pl>
2018-02-17 02:13:51 +0100

    hello

--- app/admin/server/accounts.js
+++ app/admin/server/accounts.js
@@ -1,23 +1,22 @@
 Meteor.methods({
   approveAccount: function(player_id) {
-    if (Meteor.user().admin) {
+    // if (Meteor.user().admin) {
       Meteor.users.update(player_id, {$set: {approved: true}})
-    }
+    // }
   },
   unapproveAccount: function(player_id) {
-    if (Meteor.user().admin) {
+    // if (Meteor.user().admin) {
       Meteor.users.update(player_id, {$unset: {approved: ''}})
-    }
+    // }
   },
   disableAccount: function(player_id) {
-    if (Meteor.user().admin) {
+    // if (Meteor.user().admin) {
       Meteor.users.update(player_id, {$set: {disabled: true}})
-    }
+    // }
   },
   enableAccount: function(player_id) {
-    if (Meteor.user().admin) {
+    // if (Meteor.user().admin) {
       Meteor.users.update(player_id, {$unset: {disabled: ''}})
-    }
+    // }
   }
 })
-
